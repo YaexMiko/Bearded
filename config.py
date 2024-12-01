537d0b4233f32d14c5a2069b75bd8602439fd297
YaexMiko <aloksinghmains756@gmail.com>
2024-12-01 18:48:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,15 +31,15 @@ APP_ID = int(os.environ.get("APP_ID", "20071888"))
 #your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "1c4cb9d94b23282abd9ae2a87a521b53")
 #your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002263002143"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002314636568"))
 #your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
 OWNER_ID = int(os.environ.get("OWNER_ID", "6943832981"))
 #port set to default 8080
 PORT = os.environ.get("PORT", "6596")
 #your database url mongodb only You can use mongo atlas free cloud database
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Monke:aloksingh@monke.kahqz.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Bearded:aloksingh@cluster0.uobe9.mongodb.net/?retryWrites=true&w=majority")
 #your database name
-DB_NAME = os.environ.get("DB_NAME", "Monke")
+DB_NAME = os.environ.get("DB_NAME", "Bearded")
 
 #for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
